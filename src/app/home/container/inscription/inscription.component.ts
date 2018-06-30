8fa93a97e0673280c8c1994f1f377107df12209b
jameleddine <mejri.jameleddine@gmail.com>
2018-06-30 16:58:10 +0100

    inscription control

--- src/app/home/container/inscription/inscription.component.ts
+++ src/app/home/container/inscription/inscription.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-
+user : any = {"name":"","email":"","password":""}
   constructor() { }
 
   ngOnInit() {
   }
 
+  saveUser(){
+    localStorage.setItem('user',JSON.stringify(this.user))
+  }
 }
